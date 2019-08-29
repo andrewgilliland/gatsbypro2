aca508d6c2b7685ec995d8ccd64e7128dd93ab9d
andrewgilliland <39772195+andrewgilliland@users.noreply.github.com>
2019-08-29 12:35:45 -0500

    Adds netlify headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,7 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-netlify`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
